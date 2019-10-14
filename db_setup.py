65ed8bcfc231199bfc853c526ec19fcdb68b7a26
Travis Nelson <travis.nelson@zonarsystems.com>
2019-10-14 14:18:02 -0700

    updated csv w accurate author_ids

--- db_setup.py
+++ db_setup.py
@@ -19,12 +19,12 @@ def create_tables(connection, cursor):
             id INTEGER PRIMARY KEY,
             author_id INTEGER,
             title TEXT UNIQUE,
-            isbn INTEGER UNIQUE, 
+            isbn INTEGER UNIQUE,
             year_published INTEGER,
             FOREIGN KEY (author_id) REFERENCES authors(id)
         );
         CREATE TABLE authors (
-            id INTEGER PRIMARY KEY, 
+            id INTEGER PRIMARY KEY,
             first_name TEXT,
             last_name TEXT
         );
@@ -41,7 +41,6 @@ def create_tables(connection, cursor):
     connection.commit()
 
 
-
 def populate_tables(connection, cursor):
 
     # I read that string constructors are bad practice, because they create vulnerabilites to SQL
@@ -51,22 +50,22 @@ def populate_tables(connection, cursor):
         dr = csv.DictReader(f)
         to_users_table = [(i['first_name'], i['last_name'], i['email'], i['password']) for i in dr]
     cursor.executemany("INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?);", to_users_table)
-    
+
     with open('authors.csv','r') as f:
         dr = csv.DictReader(f)
         to_authors_table = [(i['first_name'], i['last_name']) for i in dr]
     cursor.executemany("INSERT INTO authors (first_name, last_name) VALUES (?, ?);", to_authors_table)
-    
+
     with open('books.csv','r') as f:
         dr = csv.DictReader(f)
         to_books_table = [(i['author_id'], i['title'], i['isbn'], i['year_published']) for i in dr]
     cursor.executemany("INSERT INTO books (author_id, title, isbn, year_published) VALUES (?, ?, ?, ?);", to_books_table)
-    
+
     with open('wishlists.csv','r') as f:
         dr = csv.DictReader(f)
         to_wishlists_table = [(i['user_id'], i['book_id'], i['last_updated']) for i in dr]
     cursor.executemany("INSERT INTO wishlists (user_id, book_id, last_updated) VALUES (?, ?, ?);", to_wishlists_table)
-    
+
     connection.commit()
 
 
